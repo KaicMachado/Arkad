df814040faf13ea2899f77551b80c00b9be7ca43
Kaic Machado <kaicmachado2@gmail.com>
2024-04-03 19:10:28 -0300

    alterado link dos buttons dos produtos

--- src/Components/CardProduto.jsx
+++ src/Components/CardProduto.jsx
@@ -8,10 +8,7 @@ const CardProduto = ({ titulo, valor, subtitulo, src }) => {
    <h3 className="text-2xl sm:text-3xl">{titulo}</h3>
    <p className="text-xl">R$ {valor}</p>
    <button className="text-lime-500 hover:bg-lime-500 ease-in duration-200 hover:text-white  py-1 px-8 rounded-2xl text-sm border border-solid border-lime-500">
-    <a
-     href="https://www.instagram.com/direct/t/17846519828781050/"
-     target="_blank"
-    >
+    <a href="https://www.instagram.com/loja.arkad/" target="_blank">
      Comprar
     </a>
    </button>

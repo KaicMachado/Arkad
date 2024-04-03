dfda7424fde6fa909635ce357b2e6943321400f6
Kaic Machado <kaicmachado2@gmail.com>
2024-04-03 19:08:23 -0300

    adicionado novos produtos e href de links no button de cada produto

--- src/Components/ProdutosPersonalizados.jsx
+++ src/Components/ProdutosPersonalizados.jsx
@@ -3,12 +3,15 @@ import CardProduto from "./CardProduto";
 import Palmeiras from "../assets/palmeiras.png";
 import RickandMorty from "../assets/rickandmorty1.png";
 import Xmen from "../assets/x-men.png";
+import Fluminense from "../assets/fluminense.png";
+import JiuJitsu from "../assets/jiu-jitsu.png";
+import Harry from "../assets/harry.png";
 
 const ProdutosPersonalizados = () => {
  return (
   <div>
    <h1 className="text-4xl sm:text-6xl text-center my-6 md:my-7 font-semibold">
-    Produtos Personalizados
+    Produtos Temáticos
     <div>
      <ul className="flex flex-wrap gap-8 justify-center my-4 md:my-10">
       {/* Palmeiras */}
@@ -18,6 +21,15 @@ const ProdutosPersonalizados = () => {
         valor="10,00"
         subtitulo="Quando surge o alviverde imponente..."
         src={Palmeiras}
+       />
+      </li>
+      {/* Fluminense */}
+      <li>
+       <CardProduto
+        titulo="Chaveiro Fluminense"
+        valor="10,00"
+        subtitulo="Sou tricolor de coração, sou do clube tantas vezes campeão..."
+        src={Fluminense}
        />
       </li>
       {/* Rick and Morty */}
@@ -34,8 +46,26 @@ const ProdutosPersonalizados = () => {
        <CardProduto
         titulo="Chaveiro X-Men"
         valor="10,00"
-        subtitulo="“A mente é uma coisa frágil. Basta a mínima coisa para ir na direção errada.”"
+        subtitulo="A mente é uma coisa frágil. Basta a mínima coisa para ir na direção errada."
         src={Xmen}
+       />
+      </li>
+      {/* Jiu-Jitsu */}
+      <li>
+       <CardProduto
+        titulo="Chaveiro Jiu-Jitsu"
+        valor="10,00"
+        subtitulo="Para muitos o chão é o fim, para nós apenas o começo."
+        src={JiuJitsu}
+       />
+      </li>
+      {/* Harry Potter */}
+      <li>
+       <CardProduto
+        titulo="Chaveiro Harry Potter"
+        valor="10,00"
+        subtitulo="Pode se encontrar a felicidade mesmo nas horas mais sombrias, se a pessoa se lembrar de acender a luz."
+        src={Harry}
        />
       </li>
      </ul>

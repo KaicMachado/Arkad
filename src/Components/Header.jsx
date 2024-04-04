fc94b3657533fa1119b58bf4063c91149bf761c7
Kaic Machado <kaicmachado2@gmail.com>
2024-04-03 23:18:30 -0300

    alinhado nav-bar ao centro

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
  return (
-  <nav className="w-3/4 mx-auto flex justify-center sm:justify-between py-4 px-7">
+  <nav className="w-3/4 mx-auto flex justify-center items-center sm:justify-between py-4 px-7">
    <h1 className="text-3xl font-bold">Arkad</h1>
    <ul className="hidden sm:flex gap-6 font-semibold text-neutral-500">
     <li>

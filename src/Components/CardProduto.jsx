b534439e7a5b271e0ac25dc49e79748eeac4e3b3
Kaic Machado <kaicmachado2@gmail.com>
2024-04-30 23:06:11 -0300

    corrigido a dentro de button nos cards dos produtos

--- src/Components/CardProduto.jsx
+++ src/Components/CardProduto.jsx
@@ -7,11 +7,11 @@ const CardProduto = ({ titulo, valor, subtitulo, src }) => {
    <p className="font-bold text-neutral-500 text-base text-wrap">{subtitulo}</p>
    <h3 className="text-2xl sm:text-3xl">{titulo}</h3>
    <p className="text-xl">R$ {valor}</p>
-   <button className="text-lime-500 hover:bg-lime-500 ease-in duration-200 hover:text-white  py-1 px-8 rounded-2xl text-sm border border-solid border-lime-500">
-    <a href="https://www.instagram.com/loja.arkad/" target="_blank">
+   <a href="https://www.instagram.com/loja.arkad/" target="_blank">
+    <button className="text-lime-500 hover:bg-lime-500 ease-in duration-200 hover:text-white  py-1 px-8 rounded-2xl text-sm border border-solid border-lime-500">
      Comprar
-    </a>
-   </button>
+    </button>
+   </a>
   </div>
  );
 };
